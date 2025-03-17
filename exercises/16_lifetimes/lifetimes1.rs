c2e107721d931adc45baf48ecbcf059404695cc8
FairyEvilMoon <fairyevilmoon@gmail.com>
2025-03-17 09:41:13 +0400

    LAB 8 WEEK 10
    module 1,2,3 and lifetimes 1,2,3

--- exercises/16_lifetimes/lifetimes1.rs
+++ exercises/16_lifetimes/lifetimes1.rs
@@ -4,7 +4,7 @@
 // not own their own data. What if their owner goes out of scope?
 
 // TODO: Fix the compiler error by updating the function signature.
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -14,6 +14,15 @@ fn longest(x: &str, y: &str) -> &str {
 
 fn main() {
     // You can optionally experiment here.
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+    // Uncommenting the next line would cause a compiler error because string2 has gone out of scope
+    // println!("The longest string is still {}", result);
 }
 
 #[cfg(test)]
